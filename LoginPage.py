0392c4dc036e05e4050ae4a130a2e9756d1ed048
Kushal Shrestha <kushalstha04@gmail.com>
2024-06-26 18:52:36 +0545

    second commit

--- LoginPage.py
+++ LoginPage.py
@@ -30,68 +30,72 @@ def register():
 
 # Set the background colour of the main window
 
-app.configure(bg="#614BD4")
+canvas=tk.Canvas(app,bg="#614BD4",width=1366, height=300)
+canvas.pack()
 
 # top frame for the header
 
-top_frame= tk.Frame(app, bg="#614BD4")
-top_frame.pack(fill= tk.X)
+# top_frame= tk.Frame(app, bg="#614BD4")
+# top_frame.pack(fill= tk.X)
 
 #hostel name label
 
-Hostel_label= tk.Label(top_frame, text="Griha Hostel", font=("helvetica", 25, "bold"), bg="#614BD4", fg="white")
-Hostel_label.pack(side=tk.LEFT, padx=50, pady=20)
+Hostel_label= tk.Label(app, text="Griha Hostel", font=("helvetica", 20, "bold"), bg="#614BD4", fg="white")
+Hostel_label.place(x=50,y=20)
 
 # login and register button
-Login_btn= tk.Button(top_frame, text="Log In",command=login, bg="white", fg="#614BD4", padx=10, pady=5)
-Login_btn.pack(side=tk.RIGHT, padx=5)
+Login_btn= tk.Button(app, text="Log In",command=login, bg="white", fg="#614BD4", padx=10, pady=5)
+Login_btn.place(x=1280,y=20)
 
-Register_btn= tk.Button(top_frame, text="Register",command= register, bg="white", fg="#614BD4", padx=10, pady=5)
-Register_btn.pack(side=tk.RIGHT, padx=5)
-
+Register_btn= tk.Button(app, text="Register",command= register, bg="white", fg="#614BD4", padx=10, pady=5)
+Register_btn.place(x=1200,y=20)
 # middle frame for the main content
 
-middle_frame= tk.Frame(app, bg="#614BD4",width=600,height=100)
-middle_frame.pack(fill=tk.BOTH, expand=True, pady=20)
+# middle_frame= tk.Frame(app, bg="#614BD4",width=600,height=100)
+# middle_frame.pack(fill=tk.BOTH, expand=True, pady=20)
 
 
 #main headline label
 
-Headline_label= tk.Label(middle_frame, text="YOUR HOME AWAY\nFROM HOME", font=("helvetica", 30, "bold"), bg="#614BD4", fg="white")
-Headline_label.place(x=50,y=40)
+Headline_label= tk.Label(app, text="YOUR HOME AWAY\nFROM HOME", font=("helvetica", 30, "bold"), bg="#614BD4", fg="white")
+Headline_label.place(x=50,y=100)
 
 #subheadline label
 
-Subheadline_label= tk.Label(middle_frame, text="Experience Comfort In Every Corner", font=("helvetica", 14,"italic"), bg="#614BD4", fg="white")
-Subheadline_label.place(x=50,y=150)
+Subheadline_label= tk.Label(app, text="Experience Comfort In Every Corner", font=("helvetica", 14,"italic"), bg="#614BD4", fg="white")
+Subheadline_label.place(x=50,y=200)
 
 #frame for hygience, comfort and security
 
-button_frame= tk.Frame(middle_frame, bg="pink",pady=8)
-button_frame.pack(pady=200)
 
-#hygience button
+btn_canvas= tk.Canvas(app, bg="#E1DCDC",width=1050, height=50)
+btn_canvas.place(x=150,y=280)
 
-hygience_btn= tk.Button(button_frame, text="Hygience", command=hygiene, bg="white", fg="red", padx=150,pady=5)
+# hygiene button
 
-hygience_btn.pack(side=tk.LEFT,padx=10)
+hygiene_btn= tk.Button(app, text="Hygiene", command=hygiene, bg="white", fg="red", padx=80,pady=5)
+
+hygiene_btn.place(x=200,y=290)
 
 #comfort button
 
-comfort_btn= tk.Button(button_frame, text="Comfort",command=comfort, bg="white", fg="red", padx=150,pady=5)
-comfort_btn.pack(side=tk.LEFT,padx=10)
+comfort_btn= tk.Button(app, text="Comfort",command=comfort, bg="white", fg="red", padx=80,pady=5)
+comfort_btn.place(x=566,y=290)
 
-#security button
+# #security button
 
-security_btn= tk.Button(button_frame, text="Security", command=security, bg="white",fg="red", padx=150,pady=5)
-security_btn.pack(side=tk.LEFT, padx=10)
+security_btn= tk.Button(app, text="Security", command=security, bg="white",fg="red", padx=80,pady=5)
+security_btn.place(x=932,y=290)
 
 #frame for the image
 
-Image_frame= tk.Frame(app, bg="white", width=1366, height=800)
-Image_frame.pack(fill=tk.BOTH, expand=True, pady=10)
+Image_frame= tk.Frame(app, bg="white", width=500, height=300)
+Image_frame.pack(fill=tk.BOTH,expand=True)
+Image_frame.destroy()
 
 
+photo_canvas= tk.Canvas(app, bg="white", width=1366,height=400)
+photo_canvas.place(x=0,y=400)
 
 
 #load and display an actual image
@@ -102,11 +106,21 @@ Image_frame.pack(fill=tk.BOTH, expand=True, pady=10)
 # image_label= tk.Label(Image_frame,image=resized_image, bg="white")
 # image_label.pack(expand=True)
 
-my_image= Image.open("C:\\Users\\User\\OneDrive\\Desktop\\Project\\Hostel.jpg")
-resized_image= my_image.resize((1366,300))
-converted_image= ImageTk.PhotoImage(resized_image)
-my_label= tk.Label(Image_frame,image=converted_image)
-my_label.pack()
+# my_image=Image.open("C:\\Users\\User\\OneDrive\\Desktop\\Project\\Hostel.jpg")
+# resized_image= my_image.resize((1366,400))
+# converted_image= ImageTk.PhotoImage(resized_image)
+# my_label= tk.Label(my_image,image=converted_image)
+# my_label.place(x=0,y=300)
+
+
+image_path = "C:\\Users\\User\\OneDrive\\Desktop\\Project\\Hostel.jpg"  # Replace with the path to your image
+image = Image.open(image_path)
+resized_image = image.resize((1366,350))
+converted_image = ImageTk.PhotoImage(resized_image)
+my_label= tk.Label(image=converted_image, bg="white")
+my_label.place(x=0,y=380)
+
+
 
 
 mainloop()
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+Image_frame= tk.Frame(app, bg="white", width=500, height=300)
+Image_frame.pack(fill=tk.BOTH,expand=True)
